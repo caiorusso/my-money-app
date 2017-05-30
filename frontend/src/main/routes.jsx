bf2a8a92d5a6b31bb178f41f62e0cb944d9fec44
Caio Russo <caiorusso@gmail.com>
2017-05-30 14:34:34 -0300

    Início da integração com o Redux

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory }  from 'react-router'
 
-import Dashboard  from './dashboard/dashboard'
-import BillingCycle  from './billingCycle/billingCycle'
+import Dashboard  from '../dashboard/dashboard'
+import BillingCycle  from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
